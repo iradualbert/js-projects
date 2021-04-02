391a9ea6772161cf77ddbae28182af3c0619b850
iradualbert <albert.iradukunda@yahoo.com>
2021-04-02 13:37:44 +0300

    finished random-quote-generator

--- random-quote-generator/script.js
+++ random-quote-generator/script.js
@@ -1,22 +1,22 @@
-// GET ELEMENTS
-const button = document.getElementById("btn");
-const content = document.getElementById("quote");
-const author = document.getElementById("author");
+document.addEventListener("DOMContentLoaded", function () {
+	// GET ELEMENTS
+	const button = document.getElementById("btn");
+	const content = document.getElementById("quote");
+	const author = document.getElementById("author");
 
-// ADD CLICK EVENT TO THE BUTTON
-button.addEventListener("click", fetchQuote);
-
-// FUNCTION TO CALL API AND SET NEW QUOTE
-function fetchQuote() {
-	button.disabled = true;
-	button.innerText = "Loading....";
-	fetch("http://api.quotable.io/random").then((res) => {
-		res.json().then((data) => {
-			// console.log(data);
-			content.innerText = data.content;
-			author.innerText = data.author;
-			button.disabled = false;
-			button.innerText = "New Quote";
+	// FUNCTION TO CALL API AND SET NEW QUOTE
+	function fetchQuote() {
+		button.disabled = true;
+		button.innerText = "Loading....";
+		fetch("http://api.quotable.io/random").then(function (res) {
+			res.json().then(function (data) {
+				content.innerText = data.content;
+				author.innerText = data.author;
+				button.disabled = false;
+				button.innerText = "New Quote";
+			});
 		});
-	});
-};
+	}
+	// ADD CLICK EVENT TO THE BUTTON
+	button.addEventListener("click", fetchQuote);
+});
